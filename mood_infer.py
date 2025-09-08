b5206f92f0667be4e9495c12ba1e9852bddca0eb
batkid633 <cole.a.rivell@gmail.com>
2025-09-08 16:03:44 +0000

    deleting unnecessary files

--- mood_infer.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def predict_mood(texts, model, vectorizer, classes):
-    X = vectorizer.transform(texts).toarray()
-    probs = model.predict(X, verbose=0)
-    idxs = probs.argmax(axis=1)
-    labels = [classes[i] for i in idxs]
-    return probs, labels
--- treatment_infer.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def predict_treatment(patient_rows, model, scaler, classes):
-    # patient_rows: list of [age, severity_1to9, sleep_issues, anxiety]
-    X = scaler.transform(patient_rows)
-    probs = model.predict(X, verbose=0)
-    top_idx = probs.argmax(axis=1)
-    labels = [classes[i] for i in top_idx]
-    return probs, labels

5c2fb906fcffc1f7af9988593baca0a2bb14415e
batkid633 <cole.a.rivell@gmail.com>
2025-09-13 18:15:15 -0400

    Update train_mood2.py balance class weight to mitigate the oversampling

--- scripts/train_mood2.py
+++ scripts/train_mood2.py
@@ -33,7 +33,7 @@ vectorizer = TfidfVectorizer(max_features=10000)
 X_train_vec = vectorizer.fit_transform(X_train)
 X_test_vec = vectorizer.transform(X_test)
 
-clf = LogisticRegression(max_iter=1000)
+clf = LogisticRegression(class_weight="balanced",max_iter=1000)
 clf.fit(X_train_vec, y_train)
 
 # Evaluate
